96092a6c2e91cd08e2e8b435e09301e6a89f8d10
Elgen19 <elgenprestosa@gmail.com>
2024-09-26 18:24:24 +0800

    experiment 3

--- server/controllers/inviteController.js
+++ server/controllers/inviteController.js
@@ -46,7 +46,7 @@ exports.sendInvite = async (req, res) => {
 
     // Generate the signup link (you can replace with your frontend URL)
     // const signupLink = `http://localhost:3000/signup?token=${token}`;
-    const signupLink = `https://smartcity-admin.vercel.app/signup?token=${token}`;
+    const signupLink = `https://admin-smart-city.vercel.app/signup?token=${token}`;
     const mailOptions = {
       from: process.env.EMAIL_USER,
       to: email,
