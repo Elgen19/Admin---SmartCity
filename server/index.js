c0e6ef1f244ac54b6bff936c168397801ed1ee92
Elgen19 <elgenprestosa@gmail.com>
2024-11-14 17:13:31 +0800

    export app in index.js server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-// server/app.js
+// server/index.js
 require('dotenv').config(); // Load environment variables
 const express = require('express');
 const cors = require('cors'); // Import CORS
@@ -59,3 +59,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+module.exports = app;
