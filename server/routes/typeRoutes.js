1ff951d32819781ed82cbaf327691993bf35e5cc
Elgen19 <elgenprestosa@gmail.com>
2024-11-15 08:22:30 +0800

    erer

--- server/routes/typeRoutes.js
+++ server/routes/typeRoutes.js
@@ -1,9 +1,9 @@
-// routes/aiRoutes.js
+
 const express = require("express");
 const { analyzeFeedbackBasedOnType } = require("../controllers/typeClassifierController");
 const router = express.Router();
 
 // Define the route for analyzing feedback
-router.get("/analyze-feedback-type", analyzeFeedbackBasedOnType);
+router.post("/analyze-feedback-type", analyzeFeedbackBasedOnType);
 
 module.exports = router;
