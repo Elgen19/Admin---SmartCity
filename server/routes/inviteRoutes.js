32f9fd77541c46c4e57580948b6b537b4be12a1c
Elgen19 <elgenprestosa@gmail.com>
2024-11-15 10:21:01 +0800

    iejifjeir

--- server/routes/inviteRoutes.js
+++ server/routes/inviteRoutes.js
@@ -1,13 +1,13 @@
 // server/routes/inviteRoutes.js
 const express = require('express');
-const inviteController = require('../controllers/inviteController');
+const { sendInvite, verifyInvite } = require('../controllers/inviteController');
 const authenticate = require('../middleware/authMiddleware'); // Import your auth middleware
 const router = express.Router();
 
 router.use(authenticate); // Apply authentication middleware
 
 
-router.post('/send-invite', inviteController.sendInvite);
-router.get('/verify-invite', inviteController.verifyInvite);
+router.post('/send-invite', sendInvite.sendInvite);
+router.get('/verify-invite', verifyInvite.verifyInvite);
 
 module.exports = router;
